4ef19f31639fade4f55a1d0159964746f328645f
wiizeguy <rekstu@int.pl>
2025-10-22 11:09:45 +0200

    lab3 sql

---
+++ lab3.sql
@@ -0,0 +1,76 @@
+-- 1
+CREATE TABLE pracownik (
+	id_pracownika INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	imie VARCHAR(40) NOT NULL,
+	nazwisko VARCHAR(40) NOT NULL,
+	data_urodzenia DATE NOT NULL
+); -- 1.1
+
+INSERT INTO pracownik(imie, nazwisko, data_urodzenia) VALUES ('Jan', 'Nowak', '1999-07-18'), ('Janusz', 'Kowalski', '1995-05-01'), ('Andrzej', 'Zet', '1988-01-15'); -- 1.2
+
+UPDATE pracownik SET data_urodzenia='1810-08-08' WHERE id_pracownika=1; -- 1.3
+
+-- 2
+CREATE TABLE zadanie (
+	id_zadania INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	nazwa_zadania VARCHAR(150) NOT NULL,
+	priorytet ENUM('normalny', 'wysoki', 'niski'),
+	opis LONGTEXT,
+	pracownik INT,
+	FOREIGN KEY (pracownik) REFERENCES pracownik(id_pracownika) ON DELETE SET NULL
+); -- 2.1
+
+ALTER TABLE zadanie MODIFY COLUMN priorytet ENUM('normalny', 'wysoki', 'niski') DEFAULT 'normalny'; -- 2.2
+
+INSERT INTO zadanie (nazwa_zadania, pracownik) VALUES ('Zadanie1', 1), ('Zadanie2', 2), ('Zadanie3', 3); -- 2.3
+
+-- 3
+CREATE TABLE projekt (
+	id_projektu INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	nazwa_projektu VARCHAR(150) NOT NULL,
+	data_rozpoczecia DATE NOT NULL,
+	data_zakonczenia DATE
+); -- 3.1
+
+ALTER TABLE projekt ADD COLUMN menadzer_projektu INT NOT NULL, ADD CONSTRAINT fk_menadzer_projektu FOREIGN KEY (menadzer_projektu) REFERENCES pracownik(id_pracownika); -- 3.2
+
+INSERT INTO projekt(nazwa_projektu, data_rozpoczecia, menadzer_projektu) VALUES ('nazwaProj1', '1998-10-10', 1), ('nazwaProj2', '1991-04-05', 1); -- 3.3
+
+ALTER TABLE zadanie ADD COLUMN projekt INT, ADD CONSTRAINT fk_projekt FOREIGN KEY (projekt) REFERENCES projekt(id_projektu) ON DELETE CASCADE; -- 3.4
+
+UPDATE zadanie SET projekt=1; -- 3.5
+
+-- 4
+CREATE TABLE sprint (
+	id_sprintu INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	numer_sprintu INT UNSIGNED,
+	data_rozpoczecia DATE,
+	data_zakonczenia DATE
+); -- 4.1
+
+ALTER TABLE zadanie ADD COLUMN sprint INT, ADD CONSTRAINT fk_sprint FOREIGN KEY (sprint) REFERENCES sprint(id_sprintu) ON DELETE SET NULL; -- 4.2
+
+INSERT INTO sprint (numer_sprintu, data_rozpoczecia) VALUES (1337, '1995-10-10'); -- 4.3
+UPDATE zadanie SET sprint=(SELECT id_sprintu FROM sprint) WHERE id_zadania IN (1, 2); -- 4.3
+
+-- 5
+CREATE TABLE status (
+	id_statusu INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	nazwa_statusu VARCHAR(20)
+); -- 5.1
+
+INSERT INTO status(nazwa_statusu) VALUES ('ok'), ('opoznienie'), ('katastrofa'); -- 5.2
+
+CREATE TABLE zadanie_has_status (
+	id_zdarzenia INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id_zadania INT NOT NULL,
+	id_statusu INT NOT NULL,
+	data_zdarzenia DATETIME DEFAULT CURRENT_TIMESTAMP
+); -- 5.3
+
+INSERT INTO zadanie_has_status(id_zadania, id_statusu) VALUES (1, 1), (2, 2), (3, 3); -- 5.4
+
+SELECT COUNT(id_zadania) AS 'zadania_przed_delete' FROM zadanie WHERE projekt = 1; DELETE FROM projekt WHERE id_projektu = 1; SELECT COUNT(id_zadania) AS 'zadania_po_delete' FROM zadanie WHERE projekt = 1; -- 5.5
+
+-- ???
+UPDATE zadanie SET sprint = NULL; ALTER TABLE zadanie DROP FOREIGN KEY fk_sprint; DROP TABLE sprint; -- 5.6
